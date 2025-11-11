7239c25e67f91d7c9f8ccd48605a24f73db705d8
Arinarusu <145228540+Arinarusu@users.noreply.github.com>
2025-11-11 14:16:11 +0200

    Create Pr_3_UM_RA.py

---
+++ Pr_3_UM_RA.py
@@ -0,0 +1,10 @@
+import random
+for x in range (1):
+    nr1=random.randint(1,6)
+    nr2=random.randint(1,6)
+    
+print(nr1)
+print(nr2)
+
+if nr1==nr2:
+    print('Numerele sunt duble, suma lor c=',nr1+nr2)
